d7fc7b8707f8aaefc7f4ffe943df6fde3c78e38b
Jules Allemand <jules.allemandlycee@gmail.com>
2025-04-08 21:39:00 -0400

    [feature] created scriptable variable
    added default concrete for int, float, vector2, vector3, string
    custom inspector to allow current value edit only  during gameplay
    added text mesh pro

---
+++ Assets/Scripts/ScriptableVariable/ConcreteVariable/Vector2Variable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Scriptable.Variable
+{
+    [CreateAssetMenu( fileName = "Vector2Variable", menuName = "Scriptable/Variable/Vector2" )]
+    public class Vector2Variable : ScriptableVariable<Vector2> { }
+}
