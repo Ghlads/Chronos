ac90aeab3f2781f433d451698ed9599d7ab578dc
Jules Allemand <jules.allemandlycee@gmail.com>
2025-04-08 21:39:00 -0400

    [feature] created scriptable variable
    added default concrete for int, float, vector2, vector3, string
    custom inspector to allow current value edit only  during gameplay
    added text mesh pro

---
+++ Assets/Scripts/ScriptableVariable/ConcreteVariable/BoolVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Scriptable.Variable
+{
+    [CreateAssetMenu( fileName = "BoolVariable", menuName = "Scriptable/Variable/Bool" )]
+    public class BoolVariable : ScriptableVariable<bool> { }
+}
---
+++ Assets/Scripts/ScriptableVariable/Core/RuntimeScriptableObject.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scriptable.Variable
+{
+    public abstract class RuntimeScriptableObject : ScriptableObject
+    {
+        private static List<RuntimeScriptableObject> s_runtimeScriptableObjects = new List<RuntimeScriptableObject>();
+
+        private void OnEnable()
+        {
+            s_runtimeScriptableObjects.Add(this);
+        }
+
+        private void OnDisable()
+        {
+            s_runtimeScriptableObjects.Remove(this);
+        }
+
+        public abstract void RuntimeReset();
+
+        [RuntimeInitializeOnLoadMethod( RuntimeInitializeLoadType.BeforeSceneLoad )]
+        private static void ResetRuntimeScriptableObject()
+        {
+            for( int index = 0; index < s_runtimeScriptableObjects.Count; index++ )
+            {
+                RuntimeScriptableObject RuntimeObject = s_runtimeScriptableObjects[index];
+                if ( RuntimeObject == null )
+                {
+                    s_runtimeScriptableObjects.RemoveAt( index );
+                    index--;
+                    continue;
+                }
+
+                RuntimeObject.RuntimeReset();
+            }
+        }
+    }
+}
