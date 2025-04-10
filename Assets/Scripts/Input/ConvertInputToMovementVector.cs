d244ee89f42527426a219f75a68ccd96606ba1db
Jules Allemand <jules.allemandlycee@gmail.com>
2025-04-10 19:02:48 -0400

    [feature] link input to movement component
    Created Character prefab
    Created component to link pointer position to movement vector

---
+++ Assets/Scripts/Input/ConvertInputToMovementVector.cs
@@ -0,0 +1,48 @@
+using Scriptable.Variable;
+using UnityEngine;
+
+[RequireComponent( typeof( Movement2D ) )]
+public class ConvertInputToMovementVector : MonoBehaviour
+{
+    [SerializeField] private InputSource m_inputSource;
+    [SerializeField] private CameraVariable m_cameraVariable;
+    [SerializeField] private Movement2D m_movement;
+
+    private void OnEnable()
+    {
+        m_inputSource.Enable();
+    }
+
+
+    private void OnDisable()
+    {
+        m_inputSource.Disable();
+    }
+
+
+    private void Update()
+    {
+        if ( !m_inputSource.IsPointerDown )
+        {
+            return;
+        }
+
+        Vector3 worldPointerPosition = m_cameraVariable.Value.ScreenToWorldPoint( m_inputSource.PointerPosition );
+        worldPointerPosition.z = 0;
+
+        Vector3 worldPosition = transform.position;
+        worldPosition.z = 0;
+
+        m_movement.SetMovement( worldPointerPosition - worldPosition );
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if ( m_movement == null )
+        {
+            m_movement = GetComponent<Movement2D>();
+        }
+    }
+#endif // UNITY_EDITOR
+}
