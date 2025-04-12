48951d62538ba1b21a164de07c41e3fc944d8e67
Jules Allemand <jules.allemandlycee@gmail.com>
2025-04-12 00:50:25 -0400

    [feature] project overlay description
    Created fonctional UI displaying on screen project info such as title my role in it, challenges over come
    Created an auto scroller for text bigger than size given, configurable to scroll either horizontally or vertically
    Created SO containing project data
    Removed TMPro default font and replaced it with medodica, included medodica licence

---
+++ Assets/Scripts/UI/ProjectDisplayController.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProjectDisplayController : MonoBehaviour
+{
+    [SerializeField] private BulletPointController m_bulletPointPrefab;
+    [Space]
+    [SerializeField] private Button m_backButton;
+    [Space]
+    [Header( "Details Container" )]
+    [SerializeField] private Image m_splash;
+    [SerializeField] private Button m_trailerButton;
+    [SerializeField] private TextMeshProUGUI m_titleTMP;
+    [SerializeField] private TextMeshProUGUI m_platformesTMP;
+    [SerializeField] private TextMeshProUGUI m_technologiesTMP;
+    [SerializeField] private Transform m_bulletPointsContainer;
+
+    [Space]
+    [SerializeField] private ProjectData m_projectData;
+
+    private List<BulletPointController> m_bulletPoints = new();
+
+    private void Start()
+    {
+        SetProject( m_projectData );
+        m_trailerButton.onClick.AddListener( OpenTrailerURL );
+    }
+
+    public void SetProject( ProjectData projectData )
+    {
+        m_projectData = projectData;
+        m_splash.sprite = m_projectData.Splash;
+        m_titleTMP.text = $"Title : {m_projectData.Title}";
+        m_platformesTMP.text = $"Platformes : {StringUtils.Concat( m_projectData.Platformes, "/ " )}";
+        m_technologiesTMP.text = $"Technologies : {StringUtils.Concat( m_projectData.Techonologies, " / " )}";
+
+        foreach ( BulletPointController bulletPoint in m_bulletPoints )
+        {
+            Destroy( bulletPoint.gameObject );
+        }
+
+        foreach ( BulletPointData point in m_projectData.BulletPoints )
+        {
+            BulletPointController bulletPoint = Instantiate( m_bulletPointPrefab, m_bulletPointsContainer );
+            bulletPoint.SetData( point );
+        }
+    }
+
+
+    private void OpenTrailerURL()
+    {
+        Application.OpenURL( m_projectData.TrailerLink );
+    }
+}
---
+++ Assets/Scripts/UI/Scroller/Module/VerticalScrollerModule.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class VerticalScrollerModule : AxisScrollerModule
+{
+    protected override void ApplyPosition( float newPos )
+    {
+        Vector2 pos = Data.RectTransform.anchoredPosition;
+        pos.y = newPos;
+        Data.RectTransform.anchoredPosition = pos;
+    }
+
+    protected override float GetPosition()
+    {
+        return Data.RectTransform.anchoredPosition.y;
+    }
+
+    protected override float GetTotalSize()
+    {
+        return Data.TextMeshProUGUI.preferredHeight;
+    }
+
+    protected override float GetVisibleSize()
+    {
+        return Data.RectTransform.rect.height;
+    }
+}
