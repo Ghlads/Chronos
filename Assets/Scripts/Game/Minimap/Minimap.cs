e004c6ab4a8fb76eeef180783034f20107f7a374
Jules Allemand <jules.allemandlycee@gmail.com>
2025-05-21 21:27:03 -0400

    [feature] minimap behaviour
    cleaned script folders
    made a minimap behaviour

---
+++ Assets/Scripts/Game/Minimap/Minimap.cs
@@ -0,0 +1,55 @@
+using Framework.Scriptable.Generated;
+using System.Collections.Generic;
+using Unity.Hierarchy;
+using UnityEngine;
+
+namespace Game
+{
+    public class Minimap : MonoBehaviour
+    {
+        [SerializeField] private RectVariable m_mapBoundsVariable;
+        [SerializeField] private GameObjectVariable m_shipVariable;
+        [SerializeField][Tooltip( "As 1/value" )] private float m_mapScale = 5.0f;
+        [Header("Island Marker")]
+        [SerializeField] private GameObjectRuntimeSet m_islandsSet;
+        [SerializeField] private RectTransform m_islandMarkerContainer;
+        [SerializeField] private GameObject m_islandMarkerPrefab;
+
+        private readonly List<GameObject> m_islandsMarkers = new();
+
+        private void Start()
+        {
+            for ( int index = 0; index  < m_islandsSet.Count; index++ )
+            {
+                GameObject marker = Instantiate( m_islandMarkerPrefab, Vector3.zero, Quaternion.identity, m_islandMarkerContainer );
+                m_islandsMarkers.Add( marker );
+            }
+        }
+
+
+        private Vector2 TransformWorldToMapSpace( Vector3 position )
+        {
+            return TransformWorldToMapSpace( position, m_shipVariable.Value.transform.position );
+        }
+
+
+        public Vector2 TransformWorldToMapSpace( Vector3 position, Vector3 origin )
+        {
+            Vector2 offset = position - origin;
+            float offsetMagnitude = offset.magnitude;
+            Vector2 offsetDirection = offset / offsetMagnitude;
+            return offsetMagnitude * ( 1 / m_mapScale ) * offsetDirection;
+        }
+
+
+        private void Update()
+        {
+            Vector3 shipPosition = m_shipVariable.Value.transform.position;
+            for ( int index = 0; index < m_islandsMarkers.Count; index++ )
+            {
+                m_islandsMarkers[index].transform.localPosition = TransformWorldToMapSpace( m_islandsSet[index].transform.localPosition, shipPosition );
+            }
+        
+        }
+    }
+}
