f44d698257f01a24abb42a33d3dbe284f55a9b6d
Jules Allemand <jules.allemandlycee@gmail.com>
2025-06-22 23:25:58 -0400

    [FEATURE] Added Input incitation as tuto

---
+++ Assets/Framework/Script/Core/Tweening/TweenAction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Framework.Core
+{
+    [RequireComponent( typeof( Tweening ) )]
+    public abstract class TweenAction : MonoBehaviour
+    {
+        [Header( "TweenAction" )]
+        [SerializeField] private bool m_clampT = false;
+        [Header( "Debug" )]
+        [SerializeField] private bool m_debug = false;
+
+        public void Tween( float t )
+        {
+            if ( m_debug )
+            {
+                Debug.Log( $"[Tween] Debug : [{this.GetType()}] : T = {t}" );
+            }
+
+            InternalTween( m_clampT ? Mathf.Clamp01( t ) : t );
+        }
+
+        protected abstract void InternalTween( float t );
+    }
+}
